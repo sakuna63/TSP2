3e7f6f5a92b6942a3c9924d96a7928d8b07e7820
Ippei Nawate <saku.ni63@gmail.com>
2015-01-15 00:49:08 +0900

    実験8

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.CountDownLatch
 import com.github.tototoshi.csv.CSVWriter
 import common.{Calc, TSPProblem}
 import generator.NNGenerator
-import solver.{AntColonySolver, TwoOptSolver}
+import solver.{MMAntColonySolver, AntColonySolver, TwoOptSolver}
 import scala.runtime.ScalaRunTime._
 
 import scala.io.Source
@@ -14,37 +14,44 @@ import scala.util.Random
  * Created by sakuna63 on 12/23/14.
  */
 object Main {
+  val ALPHA = 1
+  val BETA = Array(2,3,4,5)
+  val RHO = 0.2
+  val P_BEST = 0.05
   val SEEDS = Array(113, 367, 647, 947, 491, 997, 839, 1733, 271, 569)
 
   def main(args: Array[String]): Unit = {
     val directory = new File("./samples")
-    directory.listFiles().foreach(f => {
-      val problem = new TSPProblem(f)
-      println(problem.name)
 
+    val eil51 = new File(directory, "eil51.tsp")
+    val problem = new TSPProblem(eil51)
+    val solver = createSolver(problem.cities.length, ALPHA, RHO, P_BEST)(SEEDS(0))
+    val optimalBeta = SEEDS.map(beta => (Calc.adjacentDis(problem, solver(beta).solve(problem)), beta)).minBy(_._1)._2
+
+//    val results = directory.listFiles().map(f => {
+    val results = for (f <- directory.listFiles()) yield {
       def average(distances: Array[Double]) = distances.sum / distances.size
       def standardDeviation(distances: Array[Double]) = {
         val ave = average(distances)
-        math.sqrt(distances.map(_-ave).map(math.pow(_, 2)).sum / distances.size)
+        math.sqrt(distances.map(_ - ave).map(math.pow(_, 2)).sum / distances.size)
       }
 
-      val m = problem.cities.length
-      val alpha = 1
-      val beta = 2
-      val rho = 0.5
+      val problem = new TSPProblem(f)
+      val solver = createSolver(problem.cities.length, ALPHA, RHO, P_BEST)(_)(beta = optimalBeta)
 
-      new Thread(new Runnable {
-        override def run(): Unit = {
-          val distances = for (seed <- SEEDS) yield (seed, new AntColonySolver(m, alpha, beta, rho, seed).solve(problem))
-          println(s"${problem.name}-${distances.minBy({case (seed, path) => Calc.adjacentDis(problem, path)})._1}")
-        }
-      }).start()
+      val result = for (seed <- SEEDS) yield (Calc.adjacentDis(problem, solver(seed).solve(problem)), seed)
+      val optimalSeed = result.minBy(_._1)._2
+      val distances = result.map(_._1).toArray
 
-//      (problem.name, distances.max, distances.min, average(distances), standardDeviation(distances))
-    })
+      (problem.name, optimalSeed, distances.max, distances.min, average(distances), standardDeviation(distances))
+    }
 
-//    val writer = CSVWriter.open("results.csv")
-//    writer.writeAll(results.map(_.productIterator.toSeq))
-//    writer.close()
+    val writer = CSVWriter.open("results.csv")
+    writer.writeAll(results.map(_.productIterator.toSeq))
+    writer.close()
+  }
+
+  def createSolver(m:Int, alpha:Int, rho:Double, bestP:Double)(seed:Long)(beta:Int) = {
+    new MMAntColonySolver(m, alpha, beta, rho, bestP, seed)
   }
 }
